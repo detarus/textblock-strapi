66e567fbf7bd4a91b290817b656f681c501dfbef
detarus <tareevlive@gmail.com>
2025-05-04 14:14:17 +0300

    Added CORS rules

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,15 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // or '*', for testing
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
